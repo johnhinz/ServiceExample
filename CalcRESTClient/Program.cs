04da0b3ecc76963ea1f1da2e89369583bccff523
jhinz <jhinz402@hotmail.com>
2019-09-09 20:54:41 -0400

    Added a wonderful REST service and client

---
+++ CalcRESTClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcRESTClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("http://localhost:51272/");
+            RestRequest request = new RestRequest("api/calc?x=14&y=15", Method.GET);
+            var response = client.Execute(request);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RESTCalcService/Controllers/CalcController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTCalcService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<int> Add(int x, int y)
+        {
+            return Ok(x + y);
+        }
+
+        public ActionResult<double> Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(x / y);
+
+
+        }
+
+
+
+    }
+}
